22165eb5d95856368c9c2e5b6e7369355a773579
bjorn-stevens <64255981+bjorn-stevens@users.noreply.github.com>
2025-08-04 21:42:06 +0200

    changed logic on data processing

--- gate_processing/make_gate_l2.py
+++ gate_processing/make_gate_l2.py
@@ -12,65 +12,68 @@ import moist_thermodynamics.functions as mt
 numcodecs.blosc.set_nthreads(1)
 
 
-def process_gate(fdir):
+def process_gate(fdir, alt_bin_centers):
     """
     convert GATE sounding data from netcdf files to a gridded product
     """
 
     es = svp.liq_wagner_pruss
+    dz = np.diff(alt_bin_centers)[0]
     #
     #
-    dz = 10
-    alt_bin_centers = np.arange(0, 31000.0 + dz / 2, dz)
     x = []
     for f in sorted(glob.glob(f"{fdir}/*.nc")):
         ds = xr.open_dataset(f)
         x.append(
             ds.groupby_bins(
-                ds.alt, bins=(alt_bin_centers - dz / 2), labels=alt_bin_centers[:-1]
+                ds.alt, bins=(alt_bin_centers) - dz / 2, labels=alt_bin_centers[:-1]
             )
             .mean()
             .assign(
-                sonde_id=(("sonde", [f], {"long_name": "sonde identifier"})),
-                platform_id=(
-                    ("sonde", [ds.platform], {"long_name": "platform identifier"})
-                ),
-                launch_lon=(
-                    (
+                {
+                    "sonde_id": (
+                        "sonde",
+                        [f[f.rfind("/") + 1 :]],
+                        {"long_name": "sonde identifier"},
+                    ),
+                    "platform_id": (
+                        "sonde",
+                        [ds.platform],
+                        {"long_name": "platform identifier"},
+                    ),
+                    "launch_lon": (
                         "sonde",
                         [np.float32(ds.launch_end_position.split()[0])],
                         {
                             "long_name": "longitude of drop end position",
                             "units": "degrees_east",
                         },
-                    )
-                ),
-                launch_lat=(
-                    (
+                    ),
+                    "launch_lat": (
                         "sonde",
                         [np.float32(ds.launch_end_position.split()[1])],
                         {
                             "long_name": "latitude of drop end position",
                             "units": "degrees_north",
                         },
-                    )
-                ),
-                launch_time=(
-                    (
+                    ),
+                    "launch_time": (
                         "sonde",
-                        ds.time.values,
+                        ds["time"].values,
                         {
                             "long_name": "launch time of the sounding",
                             "standard_name": "time",
                         },
-                    )
-                ),
+                    ),
+                },
             )
         )
 
     sondes = (
         xr.concat(x, dim="sonde")
         .drop_vars(["va_err", "ua_err", "hus_err", "ta_err"])
+        .set_coords(["launch_lat", "launch_lon"])
+        .sortby("launch_time")
         .rename(
             {
                 "alt_bins": "altitude",
@@ -81,71 +84,80 @@ def process_gate(fdir):
             }
         )
     )
-
-    sondes = sondes.assign(
-        p=(
-            sondes.p.dims,
-            (sondes.p * 100).values,
+    sondes = (
+        sondes.assign(
             {
-                "standard_name": "air_pressure",
-                "units": "Pa",
-                "long_name": "atmospheric pressure",
-            },
-        ),
-        q=(
-            sondes.q.dims,
-            (sondes.q / 1000).values,
+                "p": (
+                    sondes.p.dims,
+                    (sondes["p"] * 100).values,
+                    {
+                        "standard_name": "air_pressure",
+                        "units": "Pa",
+                    },
+                ),
+                "q": (
+                    sondes.q.dims,
+                    (sondes["q"] / 1000).values,
+                    {
+                        "standard_name": "specific_humidity",
+                        "units": "kg/kg",
+                    },
+                ),
+                "ta": (
+                    sondes.ta.dims,
+                    (sondes["ta"] + 273.15).values,
+                    {
+                        "standard_name": "air_temperature",
+                        "units": "K",
+                    },
+                ),
+            }
+        )
+        .assign(
             {
-                "standard_name": "specific_humidity",
-                "units": "kg/kg",
-                "long_name": "specific humidity",
-            },
-        ),
-        ta=(
-            sondes.ta.dims,
-            (sondes.ta + 273.15).values,
+                "rh": (
+                    sondes.q.dims,
+                    mt.specific_humidity_to_relative_humidity(
+                        sondes["q"], sondes["p"], sondes["ta"], es=es
+                    ).values,
+                    {
+                        "standard_name": "relative_humidity",
+                        "units": "1",
+                        "description": "Wagner-Pruss saturation vapor pressure over liquid",
+                    },
+                ),
+                "theta": (
+                    sondes.ta.dims,
+                    mt.theta(sondes["ta"], sondes["p"]).values,
+                    {
+                        "standard_name": "air_potential_temperature",
+                        "units": "K",
+                        "description": "calculated with dry air gas constants and 1000 hPa as standard pressure",
+                    },
+                ),
+            }
+        )
+        .assign_attrs(
             {
-                "standard_name": "air_temperature",
-                "units": "K",
-                "long_name": "air temperature",
-            },
-        ),
+                "title": "GATE radiosonde dataset (Level 2)",
+                "summary": "GATE ship based soundings mapped to a uniform 10 m grid using the groupby_bins('alt' ... ).mean() ",
+                "featureType": "profile",
+                "creator_name": "Bjorn Stevens",
+                "creator_email": "bjorn.stevens@mpimet.mpg.de",
+                "license": "CC-BY-4.0",
+                "processing_level": "2",
+                "project": "orcestra",
+                "institution": "Max Planck Institute for Meteorologie",
+                "source": "radiosonde",
+                "history": "Processed netCDF version of archived data provided by Rene Redler",
+            }
+        )
     )
-    sondes = sondes.assign(
-        rh=(
-            sondes.q.dims,
-            mt.specific_humidity_to_relative_humidity(
-                sondes.q, sondes.p, sondes.ta, es=es
-            ).values,
-            {
-                "standard_name": "relative_humidity",
-                "units": "'",
-                "long_name": "relative humidity with respect to liquid",
-                "description": "Wagner-Pruss saturation vapor pressure",
-            },
-        ),
-        theta=(
-            sondes.ta.dims,
-            mt.theta(sondes.ta, sondes.p).values,
-            {
-                "standard_name": "air_potential_temperature",
-                "units": "K",
-                "long_name": "dry potential temperature",
-            },
-        ),
-    ).set_coords(["launch_lat", "launch_lon"])
 
-    sondes.attrs = dict(
-        creator_name="Bjorn Stevens",
-        creator_email="bjorn.stevens@mpimet.mpg.de",
-        title="GATE Level2 Sounding Data",
-        license="CC-BY-4.0",
-        summary=(
-            "GATE ship-soundings mapped to a uniform 10 m grid using the groupby_bins('alt' ... ).mean() "
-        ),
-    )
+    del sondes.attrs["launch_start_position"]
+    del sondes.attrs["launch_end_position"]
 
-    return sondes.sortby("launch_time")
+    return sondes
 
 
 # %%
@@ -153,4 +165,17 @@ def process_gate(fdir):
 #
 src = "/Users/m219063/work/data/orcestra/gate/sondes/"
 fname = "/Users/m219063/data/gate-l2.zarr"
-process_gate(src).to_zarr(fname)
+
+altitude_bin_centers = np.arange(0, 3100) * 10
+process_gate(src, altitude_bin_centers).to_zarr(fname)
+#
+# ipfs files ls -l
+# ipfs pin rm -r /cid
+# ipfs add --recursive --hidden --raw-leaves --chunker=size-1048576 --quieter
+# ipfs files cp /ipfs/cid gate-l2.zarr
+
+# %%
+# gate = xr.open_zarr("ipfs://QmVkdfVm9ryi4Vn3eK6LrwC4tSLZtSkVTrUHqhLjWNv4CV")
+gate = xr.open_zarr(fname)
+gate.rh.max().values
+# %%
--- gate_processing/make_gate_l3.py
+++ gate_processing/make_gate_l3.py
@@ -36,37 +36,41 @@ def fill_gaps(ds, max_igap=1500, max_egap=300):
     }
 
     ds = ds.assign(lnp=np.log(ds.p))
-    ds = ds.assign(
-        **{
-            var: ds[var].interpolate_na(dim="altitude", method=method, max_gap=max_igap)
-            for var, method in ivars.items()
-        }
+    return (
+        ds.assign(
+            **{
+                var: ds[var].interpolate_na(
+                    dim="altitude", method=method, max_gap=max_igap
+                )
+                for var, method in ivars.items()
+            }
+        )
+        .assign(p=np.exp(ds.lnp))
+        .assign(theta=mt.theta(ds.ta, ds.p))
+        .assign(q=mt.relative_humidity_to_specific_humidity(ds.rh, ds.p, ds.ta, es=es))
+        .assign(
+            **{
+                var: ds[var].interpolate_na(
+                    dim="altitude",
+                    method=method,
+                    max_gap=max_egap,
+                    fill_value="extrapolate",
+                )
+                for var, method in evars.items()
+            }
+        )
+        .assign(p=np.exp(ds.lnp))
+        .assign(ta=mt.theta2T(ds.theta, ds.p))
+        .assign(rh=mt.specific_humidity_to_relative_humidity(ds.q, ds.p, ds.ta))
+        .drop_vars("lnp")
     )
-    ds = ds.assign(p=np.exp(ds.lnp))
-    ds = ds.assign(theta=mt.theta(ds.ta, ds.p))
-    ds = ds.assign(
-        q=mt.relative_humidity_to_specific_humidity(ds.rh, ds.p, ds.ta, es=es)
-    )
-    ds = ds.assign(
-        **{
-            var: ds[var].interpolate_na(
-                dim="altitude",
-                method=method,
-                max_gap=max_egap,
-                fill_value="extrapolate",
-            )
-            for var, method in evars.items()
-        }
-    )
-    ds = ds.assign(p=np.exp(ds.lnp))
-    ds = ds.assign(ta=mt.theta2T(ds.theta, ds.p))
-    ds = ds.assign(rh=mt.specific_humidity_to_relative_humidity(ds.q, ds.p, ds.ta))
-    ds = ds.drop_vars("lnp")
-
-    return ds
 
 
-def mask_unphysical(ds, max_alt=22000):
+def in_bnds(da, lo, hi):
+    return da.where((da > lo) & (da < hi))
+
+
+def mask_unphysical(ds):
     """remove unphyscial values from the data set as determined by physical upper and lower bounds"""
 
     physical_bnds = {
@@ -76,67 +80,106 @@ def mask_unphysical(ds, max_alt=22000):
         "v": [-20, 20],
         "p": [1000, 102500],
     }
-    x = ds.sel(altitude=slice(-10, max_alt))
-    for fld, val in physical_bnds.items():
-        x[fld] = x[fld].where((x[fld] > val[0]) & (x[fld] < val[1]))
+
+    ds = ds.assign(
+        {fld: in_bnds(ds[fld], *bnds) for fld, bnds in physical_bnds.items()}
+    )
 
     # striping is evident in the meteor sondes at about two altitudes.  this removes it
 
     for z1 in [2050, 4900]:
-        mask1 = x["rh"].sel(altitude=slice(z1, z1 + 300)).min(axis=1) < 0.2
-        mask2 = x["rh"].sel(altitude=slice(z1, z1 + 300)).max(axis=1) > 0.4
-        mask3 = x["platform_id"] == "Meteor"
-        bad_sondes = x.isel(sonde=(mask1 & mask2 & mask3))
-        mask1 = x.altitude < z1
-        mask2 = x.altitude > z1 + 300
-        x["rh"] = x["rh"].where(
-            ~ds.sonde_id.isin(bad_sondes.sonde_id.values) & (mask1 | mask2)
+        z2 = z1 + 300
+        rh_slice = ds["rh"].sel(altitude=slice(z1, z2))
+        problem_platform = ds["platform_id"] == "Meteor"
+        dry_spike_in_region = (rh_slice.min(dim="altitude") < 0.2) & (
+            rh_slice.max(dim="altitude") > 0.4
         )
-
-    return x
-
-
-def mask_outliers(ds, cntrl, nstd, dim="sonde") -> xr.Dataset:
-    """
-    Identify outliers as those points outside of the specified range of control data and set to nan.
-    """
-    for fld in ["ta", "rh", "u", "v", "p"]:
-        x_sig = cntrl[fld].std(dim=dim, ddof=1)
-        x_bar = cntrl[fld].mean(dim=dim)
-        condition = (ds[fld] > x_bar - nstd * x_sig) & (ds[fld] < x_bar + nstd * x_sig)
-        ds[fld] = ds[fld].where(condition)
+        bad_sondes = ds.isel(sonde=(problem_platform & dry_spike_in_region))
+        outside_error_region = (ds.altitude < z1) | (ds.altitude > z2)
+        not_bad_sonde = ~ds.sonde_id.isin(bad_sondes.sonde_id.values)
+        ds = ds.assign({"rh": ds["rh"].where(not_bad_sonde | outside_error_region)})
 
     return ds
 
 
+def mask_outliers(
+    ds, flds=["ta", "rh", "u", "v", "p"], nstd=3, dim="sonde") -> xr.Dataset:
+    """
+    Identify outliers as those points outside of the specified range of control data and set to nan.
+    """
+
+    def statistical_bnds(da, nstd, dim="sonde"):
+        lo = da.mean(dim=dim) - da.std(dim=dim, ddof=1) * nstd
+        hi = da.mean(dim=dim) + da.std(dim=dim, ddof=1) * nstd
+        return [lo, hi]
+
+    cntrl = ds.isel(sonde=(ds.platform_id != "METEOR").compute())
+    return ds.assign(
+        {
+            fld: in_bnds(ds[fld], *statistical_bnds(cntrl[fld], nstd, dim=dim))
+            for fld in flds
+        }
+    )
+
+
 def coverage(xx, sf=100):
+    """Return fraction of data that is not missing"""
     return sf * (xx.notnull().sum().values / np.prod(np.shape(xx.stack())))
 
 
 # %%
-# - clean and interpolate for l3 data
+# - clean and fill for l3 data
 #
-gate = dus.open_gate("Qmcc6oJjJ9gjw1WB3Kuv3v3GdhwsM7u37fj7o6CLkh4mSH")
+gate_l2_cid = "QmVkdfVm9ryi4Vn3eK6LrwC4tSLZtSkVTrUHqhLjWNv4CV"
+gate_l2 = dus.open_gate(gate_l2_cid)
+# gate_l2 = (xr.open_dataset("~/data/gate-l2.zarr", engine="zarr")
+#            .set_coords(["launch_lat", "launch_lon", "launch_time"])
+#            .swap_dims({"sonde": "launch_time"})
+#            .sel(launch_time=slice("1974-08-10", "1974-09-30"))
+#            .swap_dims({"launch_time": "sonde"})
+#            )
 
-g0 = mask_unphysical(gate).reset_coords(["launch_time", "launch_lat", "launch_lon"])
-g1 = fill_gaps(g0, max_igap=1000, max_egap=300)
-gc = g1.isel(sonde=(g1.platform_id != "METEOR").compute())
-gate_clean = g1.pipe(mask_outliers, gc, nstd=3)
+gate_l3 = (
+    gate_l2.pipe(mask_unphysical)
+    .pipe(fill_gaps, max_igap=1000, max_egap=300)
+    .pipe(mask_outliers)
+    .sel(altitude=slice(-10, 25000))
+    .assign_attrs(
+        {
+            "title": "GATE radiosonde dataset (Level 3)",
+            "summary": "GATE ship radiosondes (filled and cleaned) subsetted to ORCESTRA time of year and lower 25 km",
+            "creator_name": "Bjorn Stevens",
+            "creator_email": "bjorn.stevens@mpimet.mpg.de",
+            "license": "CC-BY-4.0",
+            "processing_level": "3",
+            "institution": "Max Planck Institute for Meteorologie",
+            "source": "radiosonde",
+            "history": f"Processed Gate level 2 radiosonde data with {gate_l2_cid} cid",
+        }
+    )
+)
 print(
-    f"GATE ta data coverage:\n "
-    f"initially = {coverage(gate.ta): .8f}%\n "
-    f"plausible = {coverage(g0.ta): .8f}%\n "
-    f"after filling & cleaning = {coverage(g1.ta):.8f}%\n "
+    f"GATE level 3 ta data coverage:\n "
+    f"initially = {coverage(gate_l3.ta): .8f}%\n "
+    f"finally = {coverage(gate_l3.ta):.8f}%\n "
 )
 
-gate_clean.attrs = dict(
-    creator_name="Bjorn Stevens",
-    creator_email="bjorn.stevens@mpimet.mpg.de",
-    title="GATE Level3 Sounding Data",
-    license="CC-BY-4.0",
-    summary=("GATE ship-soundings filled and clean during ORCESTRA period "),
-)
+gate_l3.to_zarr("~/data/gate-l3.zarr")
 
-gate_clean.to_zarr("~/data/gate-l3.zarr")
+# %%
+fld = "lnp"
+ds = gate_l2.assign(lnp=np.log(ds.p)).assign(y=ds[fld] ** 2)
+
+# %%
+gate_l2 = (
+    xr.open_dataset("~/data/gate-l2.zarr", engine="zarr")
+    .set_coords(["launch_lat", "launch_lon", "launch_time"])
+    .swap_dims({"sonde": "launch_time"})
+    .sel(launch_time=slice("1974-08-10", "1974-09-30"))
+    .swap_dims({"launch_time": "sonde"})
+)
+gate_l2
+# %%
+gate_l3["creator_name"]
 
 # %%
