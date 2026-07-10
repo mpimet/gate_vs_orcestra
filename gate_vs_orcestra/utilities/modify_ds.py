ec93015d8010dccaa3cd361e3ac5bf8a3ae2fc09
hgloeckner <helene.gloeckner@posteo.de>
2026-07-10 20:32:46 +0200

    add fct to calc histogram

--- gate_vs_orcestra/utilities/modify_ds.py
+++ gate_vs_orcestra/utilities/modify_ds.py
@@ -3,19 +3,20 @@ import numpy as np
 import moist_thermodynamics.functions as mtf
 
 
-def get_hist_of_ta(
-    da_t, da_var, var_binrange, ta_binrange=(240, 305), var_bin_num=100, ta_bin_num=200
-):
-    bins_ta = np.linspace(ta_binrange[0], ta_binrange[1], ta_bin_num)
-    bins_var = np.linspace(var_binrange[0], var_binrange[1], var_bin_num)
-    bin_name = da_var.name + "_bin"
-    ta_name = da_t.name
-    hist = histogram(da_t, da_var, bins=[bins_ta, bins_var], dim=["altitude"]).compute()
-    return (
-        ((hist * hist[bin_name]).sum(dim=bin_name) / hist.sum(bin_name))
-        .interpolate_na(dim=f"{ta_name}_bin")
-        .rename({f"{ta_name}_bin": ta_name})
+def get_hist_of_ta(ds, var, ta_binrange=(240, 305), ta_bin_num=200):
+    hist = histogram(
+        ds.ta,
+        range=ta_binrange,
+        bins=ta_bin_num,
+        dim=["altitude"],
+        weights=ds[var],
+    ) / histogram(
+        ds.ta,
+        range=ta_binrange,
+        bins=ta_bin_num,
+        dim=["altitude"],
     )
+    return hist.rename({"ta_bin": "ta"}).rename(var)
 
 
 def get_hist_of_ta_2d(
