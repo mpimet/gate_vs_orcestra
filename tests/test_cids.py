ae19b328b516818efc11710d01669a8c26701745
bjorn-stevens <64255981+bjorn-stevens@users.noreply.github.com>
2026-09-20 23:53:55 +0200

    fixed format to please ruff

--- tests/test_cids.py
+++ tests/test_cids.py
@@ -29,7 +29,7 @@ def test_open_all(cids):
     failed = []
     for name, cid in cids.items():
         if name == "orcestra":
-            continue # Skip ORCESTRA HEAD CID
+            continue  # Skip ORCESTRA HEAD CID
 
         try:
             xr.open_dataset(f"ipfs://{cid}", engine="zarr")
